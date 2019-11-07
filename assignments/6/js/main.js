e6444f73a6040541bf10632d59387536acaf9075
Espaillat <victor_espaillat@student.uml.edu>
2019-11-07 13:07:19 -0500

    Starting assignment 6

---
+++ assignments/6/js/main.js
@@ -0,0 +1,22 @@
+/*
+File: index.html
+91.461 Assignment: Creating an Interactive Dynamic Table
+Victor M Espaillat, UMass Lowell Computer Science, victor_espaillat@student.uml.edu
+*/
+
+var multiplier_start = 1;
+var multiplier_end = 5;
+var multiplicand_start = 1;
+var multiplicand_end = 5;
+var el = document.getElementById('table');
+
+var msg = '<p>';
+
+for (i = multiplier_start; i < multiplier_end; ++i) {
+    for (j = )
+
+}
+
+var msg += '</p>';
+
+// el.innerHTML = msg;
