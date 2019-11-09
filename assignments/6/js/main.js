1834db3c1e3ba7223927af9d6aa4fd3877b9514e
Victor Espaillat <vicander_99@hotmail.com>
2019-11-08 22:56:58 -0500

    Interactive Dynamic Table
    -First draft of Interactive Dynamic Table.
    -Added link into "assigns" list.

--- assignments/6/js/main.js
+++ assignments/6/js/main.js
@@ -1,47 +1,108 @@
 /*
-File: index.html
+File: main.js
 91.461 Assignment: Creating an Interactive Dynamic Table
 Victor M Espaillat, UMass Lowell Computer Science, victor_espaillat@student.uml.edu
 */
 
+var inputs = [];   // plier1, plier2, cand1, cand2
+var valid = true;
 
-var multiplicand_start = 1;
-var multiplicand_end = 4;
+function validate(num_inputs) {
+    inputs = [];
+    valid = true;
 
-var multiplier_start = 3;
-var multiplier_end = 5;
+    console.log("validate(" + num_inputs + ")");
 
-var multiplicand = [];
-var multiplier = [];
+    // Get the value of the input fields.
+    var i;
+    for (i = 1; i <= num_inputs; ++i){
+        // The ith hidden message element.
+        el = document.getElementById("msg" + i);
 
-var i, j;
+        // Retrieve value from form and store into a variable.
+        inputs.push( parseInt(document.getElementById("input" + i).value, 10) );
 
-for (i = multiplier_start; i <= multiplier_end; ++i) {
-    multiplier.push(i);
-}
-
-for (i = multiplicand_start; i <= multiplicand_end; ++i) {
-    multiplicand.push(i);
-}
-
-console.log(multiplicand);
-console.log(multiplier);
-
-var el = document.getElementById('table');
-var msg = '<p>&emsp;&nbsp;';
-
-for (i = 0; i < multiplicand.length; ++i)
-    msg += multiplicand[i] + '&emsp;';
-msg += '<br>';
-
-for (i = 0; i < multiplier.length; ++i) {
-    msg += multiplier[i] + '&emsp;';
-    for (j = 0; j < multiplicand.length; ++j) {
-        msg += multiplier[i] * multiplicand[j] + '&emsp;';
+        console.log(inputs[i-1]);
+        if (isNaN(inputs[i-1])) {
+            el.innerHTML = "Integer required";
+            valid = false;
+        } else {
+            el.innerHTML = "<br>";
+        }
     }
-    msg += '<br>';
+
+    return valid;
+}
+
+var plier_range = []; // Multipliers, top row of table
+var cand_range = [];  // Multiplicands, leftmost column of table
+
+function generateTable(num_inputs) {
+
+    plier_range = [];
+    cand_range = [];
+
+    if (validate(num_inputs)) {
+        console.log("Great");
+
+        plier_range.push(inputs[0]);
+        plier_range.push(inputs[1]);
+        cand_range.push(inputs[2]);
+        cand_range.push(inputs[3]);
+
+        if (plier_range[0] > plier_range[1])
+            [plier_range[1], plier_range[0]] = [plier_range[0], plier_range[1]];
+
+        if (cand_range[0] > cand_range[1]) {
+            console.log(cand_range[0], cand_range[1]);
+
+            [cand_range[1], cand_range[0]] = [cand_range[0], cand_range[1]];
+        }
+
+        console.log(plier_range);
+        console.log(cand_range);
+
+        var i, j, msg;
+
+        /********************** The multpliers row ****************************/
+        msg = "<thead><tr><th scope=\"col\"></th>";
+        for (i = plier_range[0]; i <= plier_range[1]; ++i){
+            msg += "<th scope=\"col\">" + i + "</th>";
+        }
+        msg += "</tr></thead><tbody>";
+
+        /*********************** Every other row ******************************/
+        for (j = cand_range[0]; j <= cand_range[1]; ++j) {
+            msg += "</tr><th scope=\"row\">" + j + "</th>";
+            for (i = plier_range[0]; i <= plier_range[1]; ++i) {
+                msg += "<td>" + (i*j) + "</td>";
+            }
+            msg += "</tr>";
+        } 
+        msg += "</tbody>"
+
+        document.getElementById("mult-tab").innerHTML = msg;
+
+    } else {
+        console.log(":(");
+    }
 }
 
 
-msg += '</p>';
-el.innerHTML = msg;
+
+
+
+
+
+
+
+//// for (i = 0; i < multiplier.length; ++i) {
+////     msg += multiplier[i] + "&emsp;";
+////     for (j = 0; j < multiplicand.length; ++j) {
+////         msg += multiplier[i] * multiplicand[j] + "&emsp;";
+////     }
+////     msg += "<br>";
+//// }
+
+
+
