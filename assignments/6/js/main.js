9ca987142421b0a0deaa1aa43bd3d4767fb4d674
Victor Espaillat <vicander_99@hotmail.com>
2019-11-10 01:31:15 -0500

    2nd draft
    - Styled table
    - Improved form input validation
    - Documented code

--- assignments/6/js/main.js
+++ assignments/6/js/main.js
@@ -1,108 +1,102 @@
-/*
-File: main.js
-91.461 Assignment: Creating an Interactive Dynamic Table
-Victor M Espaillat, UMass Lowell Computer Science, victor_espaillat@student.uml.edu
-*/
+/******************************************************************************
+ **    File: main.js                                                         **
+ **                                                                          **
+ **    91.461 Assignment: Generating an Interactive Dynamic Table            **
+ **                                                                          **
+ **    Victor M Espaillat, UMass Lowell Computer Science,                    **
+ **    victor_espaillat@student.uml.edu                                      **
+ ******************************************************************************/
 
-var inputs = [];   // plier1, plier2, cand1, cand2
-var valid = true;
+var inputs = [];       // multiplier1, multiplier2, multiplicand1, multiplicand2
+var plier_range = [];  // Multipliers, top row of table.
+var cand_range = [];   // Multiplicands, leftmost column of table.
 
+/******************************************************************************
+ **    Attempts to retrive a given number of inputs and store them in        **
+ **    a global inputs array.                                                **
+ **                                                                          **
+ **    Then, determines whether the given inputs are valid by returning      **
+ **    true or false.                                                        **
+ **                                                                          **
+ **    Assumes that the inputs come from elements with IDs of "input#"       **
+ **    where # is an integer between 1 and num_inputs.                       **
+ ******************************************************************************/
 function validate(num_inputs) {
+    console.log("validate(num_inputs = " + num_inputs + ");");
+    /************************* Resetting variables ****************************/
     inputs = [];
-    valid = true;
+    var valid = true;
+    /************************ Retrieving form data ****************************/
+    var i, hidden_el;
+    for (i = 0; i < num_inputs; i += 2) {
+        /* Retrieve the values from i-th and (i+1)-th input elements and store 
+           them into an array. */
+        inputs.push(parseInt(document.getElementById("input"+(i+1)).value, 10));
+        inputs.push(parseInt(document.getElementById("input"+(i+2)).value, 10));
+        /*********************** Validing form data ***************************/
+        /* There will only be one message for every two input elements.
+           Retrieve the particular hidden element for this pair of inputs. */
+        hidden_el = document.getElementById("hidden_error_msg" + ((i/2)+1));
 
-    console.log("validate(" + num_inputs + ")");
-
-    // Get the value of the input fields.
-    var i;
-    for (i = 1; i <= num_inputs; ++i){
-        // The ith hidden message element.
-        el = document.getElementById("msg" + i);
-
-        // Retrieve value from form and store into a variable.
-        inputs.push( parseInt(document.getElementById("input" + i).value, 10) );
-
-        console.log(inputs[i-1]);
-        if (isNaN(inputs[i-1])) {
-            el.innerHTML = "Integer required";
+        /* If either input element of the current pair is "Not a Number" then 
+           display an error message through the hidden element. */
+        if (isNaN(inputs[i]) || isNaN(inputs[i+1])) {
+            // Reveal the "hidden" element by writting text into it.
+            hidden_el.innerHTML = "<mark class=\"px-3\">all input required !</mark>";
             valid = false;
-        } else {
-            el.innerHTML = "<br>";
+        }  else {
+            // Otherwise remain "hidden";
+            hidden_el.innerHTML = "<br>";
         }
     }
-
     return valid;
 }
 
-var plier_range = []; // Multipliers, top row of table
-var cand_range = [];  // Multiplicands, leftmost column of table
-
+/******************************************************************************
+ **    If the numbers read from a form are valid:                            **
+ **                                                                          **
+ **        Uses them to generate a multiplication table. Displays the        **
+ **        table at a desired location, as indicated by an element ID.       **
+ ******************************************************************************/
 function generateTable(num_inputs) {
-
+    /************************* Resetting variables ****************************/
     plier_range = [];
     cand_range = [];
-
-    if (validate(num_inputs)) {
-        console.log("Great");
-
-        plier_range.push(inputs[0]);
-        plier_range.push(inputs[1]);
-        cand_range.push(inputs[2]);
-        cand_range.push(inputs[3]);
-
-        if (plier_range[0] > plier_range[1])
-            [plier_range[1], plier_range[0]] = [plier_range[0], plier_range[1]];
-
-        if (cand_range[0] > cand_range[1]) {
-            console.log(cand_range[0], cand_range[1]);
-
-            [cand_range[1], cand_range[0]] = [cand_range[0], cand_range[1]];
-        }
-
-        console.log(plier_range);
-        console.log(cand_range);
-
-        var i, j, msg;
-
-        /********************** The multpliers row ****************************/
-        msg = "<thead><tr><th scope=\"col\"></th>";
-        for (i = plier_range[0]; i <= plier_range[1]; ++i){
-            msg += "<th scope=\"col\">" + i + "</th>";
-        }
-        msg += "</tr></thead><tbody>";
-
-        /*********************** Every other row ******************************/
-        for (j = cand_range[0]; j <= cand_range[1]; ++j) {
-            msg += "</tr><th scope=\"row\">" + j + "</th>";
-            for (i = plier_range[0]; i <= plier_range[1]; ++i) {
-                msg += "<td>" + (i*j) + "</td>";
-            }
-            msg += "</tr>";
-        } 
-        msg += "</tbody>"
-
-        document.getElementById("mult-tab").innerHTML = msg;
-
-    } else {
-        console.log(":(");
+    /******************* Making sure the inputs are valid *********************/
+    if (!validate(num_inputs)) {
+        console.log("Failed validation :(");
+        return;
     }
+    console.log("Validatation successful\nGenerating Table");
+    /*********** Inserting the validated inputs into range arrays *************/
+    plier_range.push(inputs[0]);
+    plier_range.push(inputs[1]);
+    cand_range.push(inputs[2]);
+    cand_range.push(inputs[3]);
+    /******* If given in incorrect order, re-arrange the input numbers ********/
+    if (plier_range[0] > plier_range[1])
+        [plier_range[1], plier_range[0]] = [plier_range[0], plier_range[1]];
+    if (cand_range[0] > cand_range[1])
+        [cand_range[1], cand_range[0]] = [cand_range[0], cand_range[1]];
+    /* Printing the input numbers out using the browser console to ensure that 
+       I'm reading them correctly. */
+    console.log("Multipliers: " + plier_range);
+    console.log("Multiplicands: " + cand_range);
+    /********************** Generate the multpliers row ***********************/
+    var i, j, table_html;
+    table_html = "<tr><th></th>";
+    for (i = plier_range[0]; i <= plier_range[1]; ++i){
+        table_html += "<th class=\"align-middle\">" + i + "</th>";
+    }
+    table_html += "</tr>";
+    /********************* Generate the rest of the rows **********************/
+    for (j = cand_range[0]; j <= cand_range[1]; ++j) {
+        table_html += "</tr><th class=\"align-middle\">" + j + "</th>";
+        for (i = plier_range[0]; i <= plier_range[1]; ++i) {
+            table_html += "<td class=\"align-middle\">" + (i*j) + "</td>";
+        }
+        table_html += "</tr>";
+    } 
+    // Insert the HTML we generated for our table into the desired element.
+    document.getElementById("mult-table").innerHTML = table_html;
 }
-
-
-
-
-
-
-
-
-
-//// for (i = 0; i < multiplier.length; ++i) {
-////     msg += multiplier[i] + "&emsp;";
-////     for (j = 0; j < multiplicand.length; ++j) {
-////         msg += multiplier[i] * multiplicand[j] + "&emsp;";
-////     }
-////     msg += "<br>";
-//// }
-
-
-
