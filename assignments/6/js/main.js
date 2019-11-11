a292cf5710368866178afc0b130fc55ae5223d7f
Victor Espaillat <vicander_99@hotmail.com>
2019-11-10 20:59:37 -0500

    Patch 1.1
    -Improved window sizing for small screens.
    -Improved error handling

--- assignments/6/js/main.js
+++ assignments/6/js/main.js
@@ -15,38 +15,49 @@ var cand_range = [];   // Multiplicands, leftmost column of table.
 **    Attempts to retrive a given number of inputs and store them in          **
 **    a global inputs array.                                                  **
 **                                                                            **
-**    Then, determines whether the given inputs are valid by returning        **
+**    Determines whether the given inputs are valid by returning              **
 **    true or false.                                                          **
+**                                                                            **
+**    Writes error messages to an empty element "hidden_error#"               **
 **                                                                            **
 **    Assumes that the inputs come from elements with IDs of "input#"         **
 **    where # is an integer between 1 and num_inputs.                         **
 *******************************************************************************/
 function validate(num_inputs) {
     console.log("validate(num_inputs = " + num_inputs + ");");
-    /************************* Resetting variables*****************************/
-    inputs = [];
-    var valid = true;
-    /************************ Retrieving form data*****************************/
-    var i, hidden_el;
-    for (i = 0; i < num_inputs; i += 2) {
-        /* Retrieve the values from i-th and (i+1)-th input elements and store 
-           them into an array. */
-        inputs.push(parseInt(document.getElementById("input"+(i+1)).value, 10));
-        inputs.push(parseInt(document.getElementById("input"+(i+2)).value, 10));
-        /*********************** Validing form data****************************/
-        /* There will only be one message for every two input elements.
-           Retrieve the particular hidden element for this pair of inputs. */
-        hidden_el = document.getElementById("hidden_error_msg" + ((i/2)+1));
-
-        /* If either input element of the current pair is "Not a Number" then 
-           display an error message through the hidden element. */
-        if (isNaN(inputs[i]) || isNaN(inputs[i+1])) {
-            // Reveal the "hidden" element by writting text into it.
-            hidden_el.innerHTML = "<mark class=\"px-3\">all input required !</mark>";
+    
+    inputs = [];            // Reset globals.
+    var inputValid = [];    // To know which hidden error element to reveal.
+    var valid = true;       // Return value
+    
+    /******************* Data retrieval and validation ************************/
+    var i, curVal;
+    for (i = 0; i < num_inputs; i++) {
+        curVal = parseFloat(document.getElementById("input" + (i+1)).value);
+        console.log("curVal" + i + ": " + curVal);
+        // Make sure input is not "NaN" or a float, but rather a proper integer.
+        if (Number.isInteger(curVal)) {
+            inputValid.push(true);
+            inputs.push(curVal);
+        } else {
+            inputValid.push(false);
+            /* If any one input is invalid, then the set is useless for our
+               multiplication table, thus validation should fail. */
             valid = false;
-        }  else {
-            // Otherwise remain "hidden";
+        }
+    }
+    
+    /**************** Display error messages if necessary *********************/
+    var hidden_el;
+    for (i = 0; i < num_inputs; i += 2) {
+        // Treat the inputs as pairs. Only display one message per pair.
+        hidden_el = document.getElementById( "hidden_error" + ((i/2)+1) );
+        if (inputValid[i] && inputValid[i+1]) {
+            // If input is valid, clear any previously written error messages.
             hidden_el.innerHTML = "<br>";
+        } else {
+            // Otherwise, write an error message.
+            hidden_el.innerHTML = "<mark class=\"px-3\">invalid input !</mark>";
         }
     }
     return valid;
@@ -59,29 +70,34 @@ function validate(num_inputs) {
 **    table at a desired location, as indicated by an element ID.             **
 *******************************************************************************/
 function generateTable(num_inputs) {
-    /************************* Resetting variables*****************************/
+    /************************** Resetting globals *****************************/
     plier_range = [];
     cand_range = [];
-    /******************* Making sure the inputs are valid**********************/
+    
+    /******************* Making sure the inputs are valid *********************/
     if (!validate(num_inputs)) {
         console.log("Failed validation :(");
         return;
     }
     console.log("Validatation successful\nGenerating Table");
+   
     /*********** Inserting the validated inputs into range arrays *************/
     plier_range.push(inputs[0]);
     plier_range.push(inputs[1]);
     cand_range.push(inputs[2]);
     cand_range.push(inputs[3]);
+    
     /******* If given in incorrect order, re-arrange the input numbers ********/
     if (plier_range[0] > plier_range[1])
         [plier_range[1], plier_range[0]] = [plier_range[0], plier_range[1]];
     if (cand_range[0] > cand_range[1])
         [cand_range[1], cand_range[0]] = [cand_range[0], cand_range[1]];
+    
     /* Printing the input numbers out using the browser console to ensure that 
        I'm reading them correctly. */
-    console.log("Multipliers: " + plier_range);
-    console.log("Multiplicands: " + cand_range);
+    console.log("Multipliers: [" + plier_range + "]");
+    console.log("Multiplicands: [" + cand_range + "]");
+    
     /********************** Generate the multpliers row ***********************/
     var i, j, table_html;
     table_html = "<tr><th></th>";
@@ -89,6 +105,7 @@ function generateTable(num_inputs) {
         table_html += "<th class=\"align-middle\">" + i + "</th>";
     }
     table_html += "</tr>";
+   
     /********************* Generate the rest of the rows **********************/
     for (j = cand_range[0]; j <= cand_range[1]; ++j) {
         table_html += "</tr><th class=\"align-middle\">" + j + "</th>";
@@ -97,6 +114,7 @@ function generateTable(num_inputs) {
         }
         table_html += "</tr>";
     } 
+    
     // Insert the HTML we generated for our table into the desired element.
     document.getElementById("mult-table").innerHTML = table_html;
 }
