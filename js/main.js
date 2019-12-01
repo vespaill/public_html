1ba0173714fa56ff8d867f94a7c92c6a4b333aaf
Victor Espaillat <vicander_99@hotmail.com>
2019-12-01 14:44:20 -0500

    Javascript plugin patch 2
    -Moved script from bottom of body to head.
    -Now it makes sense to have 'onload' event listener to load home page.

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@
 
 var debugging = false;
 
+// To hold the multiple pages' content.
 var content = {
     intro : {
         'home.'     : "Welcome, my name is Victor M. Espaillat. I&rsquo;m a computer science major at UMass Lowell, and I am also passionate about strength training and nutrition.",
         'class.'    : "Here we have my assignments from COMP.4610: <i>&ldquo;Graphical User Interface Programming I.&rdquo;</i>",
         'projects.' : "These are two collaborative projects from my project sequence.<br>The first from COMP.4110: <i>&ldquo;Software Engineering I,&rdquo;</i><br>the second from COMP.4120: <i>&ldquo;Software Engineering II.&rdquo;</i>"
     },
-
     main : {
         'home.'     : "<div id='interests'><h2>interests.</h2><a href='#' onclick='return!1'><article><img src='media/images/cacao.jpg' alt='cacao'><p>Nutrition</p></article></a><a href='#' onclick='return!1'><article><img src='media/images/desktop.jpg' alt='desktop'><p>PC Gaming</p></article></a><a href='#' onclick='return!1'><article><img src='media/images/darksouls.jpg' alt='darksouls'><p>Soulsbornekiro</p></article></a><a href='#' onclick='return!1'><article><img src='media/images/deadlift.png' alt='deadlift'><p>Strength Training</p></article></a></div>",
         'class.'    : "<div class='list'><ol><li>Setting Up to Take This Course</li><li>Creating Your First Web Page</li><a href='assignments/3/index.html'><li>Style a Site with External CSS</li></a><li>Styling Your First Web Page With CSS</li><li>Intro to GitHub</li><a href='assignments/6/index.html'><li>Interactive Dynamic Table</li></a></ol></div><div class='intro border-bottom border-top'><p>And here we have my in-class exersices.</p></div><div class='list'><ol><a href='in_class_exercises/6/example.html'><li value='6'>Modifying DOM elements</li></a></ol></div>",
@@ -25,12 +25,12 @@ var content = {
 
 /*******************************************************************************
 **    Input:                                                                  **
-**        el_ul  - an <ul> element,                                           **
-**        target - an <a> element                                             **
+**         el_ul: an <ul> element,                                            **
+**        target: an <a> element                                              **
 **                                                                            **
 **    Output:                                                                 **
 **        The class of every <a> within each <li> in el_ul will be removed.   **
-**        The target <a> element will be given a class of 'current'           **
+**        The target <a> element will be given a class of 'current.'          **
 *******************************************************************************/
 function updateCurrent(el_ul, target) {
     if (debugging) {
@@ -44,19 +44,19 @@ function updateCurrent(el_ul, target) {
         if (el_ul.children[i].firstChild.hasAttribute('class'))
             el_ul.children[i].firstChild.removeAttribute('class');
     }
-    /* Add an attribute of class with value of 'current' to our target navbarl link
-       Our CSS will then highlight this navbar link. */
+    /* Add an attribute of class with value of 'current' to our target navbar
+       link. Our CSS will then highlight this navbar link. */
     target.setAttribute('class', 'current');
-
 }
 
 /*******************************************************************************
 **    Input:                                                                  **
-**        e - event, or event target                                          **
+**        e: event, or event target                                           **
 **                                                                            **
 **    Output:                                                                 **
 **        The intro and main divs will be updated according to the given      **
-**        navbar link element, assumed to be the target of the given event e. **
+**        navbar link element, assumed to be the target of the given event    **
+**        e.                                                                  **
 *******************************************************************************/
 function updateContent(e) {
     var elIntro = document.getElementsByClassName('intro')[0];
@@ -86,7 +86,7 @@ function updateContent(e) {
 
     // Update content based on clicked navbar item.
     if (content.intro[targetText]) {                // If the content is valid
-        if (debugging) console.log('Valid nav element clicked');
+        if (debugging) console.log('Valid nav element');
         updateCurrent(el_ul, target);
         elIntro.innerHTML = '<p>' + content.intro[targetText] + '</p>';
         elMain.innerHTML = content.main[targetText];
@@ -100,20 +100,32 @@ function updateContent(e) {
     }
 }
 
-// Get the first navbar link.
-var el_first_nav_el_link = document.getElementById('navbar').firstChild.nextSibling.firstChild;
-if (debugging) {
-    console.log('el_first_nav_el_link: ');
-    console.log(el_first_nav_el_link);
-}
+/*******************************************************************************
+**    Selects the first navbar link (the home link) and updates the page      **
+**    accordingly.                                                            **
+**                                                                            **
+**    Sets up event listener for the navbar. Based on the specific navbar     **
+**    link that has been clicked, the page is updated accordingly.            **
+*******************************************************************************/
+function setup() {
+    // Get the first navbar link.
+    var el_first_nav_el_link
+        = document.getElementById('navbar').firstChild.nextSibling.firstChild;
+    updateContent(el_first_nav_el_link);
+
+    if (debugging) {
+        console.log('el_first_nav_el_link: ');
+        console.log(el_first_nav_el_link);
+    }
+
+    // Set up event listener to call updateContent() on click
+    var el = document.getElementById('navbar'); // Get navbar list
+    el.addEventListener('click', function(e) {  // Add listener on click
+        updateContent(e);                       // It calls updateContent()
+    }, false);                                  // Use bubbling phase for flow
+}/********************************* End setup *********************************/
 
 // Initialize the page to start on the 'home.' page.
 window.addEventListener('load', function() {
-    updateContent(el_first_nav_el_link);
+    setup();
 }, false);
-
-// Set up event listeners to call updateContent() on click
-var el = document.getElementById('navbar');     // Get navbar list
-el.addEventListener('click', function(e) {      // Add listener on click
-    updateContent(e);                           // It calls updateContent()
-}, false);                                      // Use bubbling phase for flow
