75c01aa38652c0a32d2c40fedec48a396429c696
Victor Espaillat <vicander_99@hotmail.com>
2019-12-06 14:33:22 -0500

    Update

--- assignments/7/js/main.js
+++ assignments/7/js/main.js
@@ -10,42 +10,87 @@
 
 var debugging = true;
 
+/* When the page is ready, the function inside the parentheses of the .ready()
+   method is run. */
 $(document).ready(function() {
 
-   jQuery.validator.setDefaults({
-        success:  "valid"
+    jQuery.validator.setDefaults({
+        success:  'valid'
     });
 
-    var form = $("#group-form");
-    form.validate({
+    var $form = $('#form-group');
+
+    $form.validate({
         rules: {
             num_field: {
                 required: true,
-                digits: true
+                range: [-50, 50]
             }
-        }
-    });
+        },
 
-    $("#submit-btn").on('click', function() {
-        if (form.valid() == true ) {
+        submitHandler: function(form) {
             generateTable();
+        },
+
+        invalidHandler: function(form) {
+            $('#mult-table').empty();
         }
     });
-
 });
 
 function generateTable() {
 
-    var arr_valid_inputs = [];
+    var arr_valid_inputs = [],
+        i, j;
 
-    // Push each of the inputs into arr.
+    // Push each of the input values into arr.
     $('input').each(function() {
         arr_valid_inputs.push(  parseInt( $(this).val(), 10 )  );
     });
 
-    console.log(arr_valid_inputs);
+    // Sort pairs
+    arr_valid_inputs = sort_pairs(arr_valid_inputs);
 
-    // $('#mult-table').append()
+    /* Store a reference of the table object inside a variable since it will be
+       used often. */
+    var $table = $('#mult-table');
+    $table.empty();             // Clear the table
 
+    var $row = $('<tr>');       // Create an empty table row element
+    $row.append('<th>');        // Append an empty table header element
+
+    // Generate the multipliers row
+    for (var i = arr_valid_inputs[0]; i <= arr_valid_inputs[1]; ++i) {
+        $row.append('<th>' + i + '</th>');
+    }
+    $table.append($row);        // Append row to table
+
+    // Generate the rest of the table, row by row
+    for (var j = arr_valid_inputs[2]; j <= arr_valid_inputs[3]; ++j) {
+
+        $row = $('<tr>');       // Create another empty table row element
+
+        // Start each row with the respective multiplicand.
+        $row.append('<th>' + j + '</th>');
+
+        // Generate the products between multipliers and multiplicands.
+        for (var i = arr_valid_inputs[0]; i <= arr_valid_inputs[1]; ++i) {
+            $row.append('<td>' + (j*i) + '</td>');
+        }
+        $table.append($row);    // Append row to table
+
+    }
+
+}
+
+function sort_pairs(arr) {
+
+    for (var i = 0; i < arr.length; i+=2) {
+        if (arr[i] > arr[i+1]) {
+            [arr[i+1], arr[i]] = [arr[i], arr[i+1]];
+        }
+    }
+
+    return arr;
 
 }
