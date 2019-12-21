86379a1a2762ab208a9305855e624214abc8c866
Victor Espaillat <vicander_99@hotmail.com>
2019-12-21 14:17:29 -0500

    Assign 9 update
    - Started scoreboard implementation
    - Scores are calculated from letter values and board multipliers.
    - Need to fix word detection: gaps between letters are not considered.
    - Need to add functionality to "Next Word" and "Reset" buttins

--- assignments/9/js/custom.js
+++ assignments/9/js/custom.js
@@ -7,71 +7,108 @@
 **    victor_espaillat@student.uml.edu                                        **
 *******************************************************************************/
 
-"use strict"
+'use strict'
 
 var debugging = false;
 
 $(document).ready(function() {
 
-    // Generate strip
-    var $blank = $('<div>').addClass('board-blank droppable ui-widget-header');
-    var $double = $blank.clone().addClass('board-double').removeClass('board-blank');
+    // Generate board
+    var $blank = $('<div>').addClass('board-blank slot droppable ui-widget-header')
+                           .attr('letter-mult', 1)
+                           .attr('word-mult', 1);
 
-    $('#strip')
-        .append($blank.clone())
-        .append($double.clone())
-        .append($blank.clone())
-        .append($blank.clone())
-        .append($blank.clone())
-        .append($double.clone())
-        .append($blank.clone());
+    var $doublew = $blank.clone().addClass('board-double-word').removeClass('board-blank');
+    var $doublel = $blank.clone().addClass('board-double-letter').removeClass('board-blank');
+    $doublew.attr('word-mult', 2);
+    $doublel.attr('letter-mult', 2);
+    var k = 0;
+    $('#board')
+        .append($blank.clone().attr('col', k++))
+        .append($doublew.clone().attr('col', k++))
+        .append($blank.clone().attr('col', k++))
+        .append($doublel.clone().attr('col', k++))
+        .append($blank.clone().attr('col', k++))
+        .append($doublew.clone().attr('col', k++))
+        .append($blank.clone().attr('col', k++));
 
     // Generate rack
-    var RACK_NUM_TILES = 6, i;
+    var RACK_NUM_TILES = 7, i;
     var $rack = $('#rack');
-    var $tile = $('<div>').addClass('draggable ui-widget-content');
-
+    var $tile = $('<img>').addClass('tile draggable ui-widget-content');
     for (i = 0; i < RACK_NUM_TILES; ++i) {
         var $newTile = $tile.clone();
-        var url = "url('images/tiles/Scrabble_Tile_" + ObjScrabble.getRandLett() + ".jpg')";
-        if (debugging) console.log(url);
-        $newTile.css({
-            'background-image': url,
-            'background-size': 'contain',
-        });
+        var key = ObjScrabble.getRandLett();
+        $newTile.attr('value', ObjScrabble.dictTiles[key].value)
+                .attr('letter', key);
+        var strSrc = 'images/tiles/Scrabble_Tile_' + key + '.jpg';
+        if (debugging) console.log(strSrc);
+        $newTile.attr('src', strSrc)
         $('#rack').append($newTile);
     }
 
-    $(".draggable").draggable({
-        scroll: false,
+    $('.tile').draggable({
         revert: true,
-        revertDuration: 200,
-        start: function() {
-            $(this).css("z-index", 2);
+        revertDuration: 100,
+        start: function (e, ui) {
+            $(this).addClass('hovering');
         },
-        stop: function() {
-            $(this).css("z-index", 1);
-        },
+        stop: function (e, ui) {
+            $(this).removeClass('hovering');
+        }
     });
 
-    $(".droppable").droppable({
-        tolerance: "intersect",
-        hoverClass: "drop-hover",
-        drop: function(e, ui) {
+    $('.slot').droppable({
+        tolerance: 'intersect',
+        hoverClass: 'drop-hover',
+        drop: function (event, ui) {
             var $this = $(this);
-            if ($this.children().length == 0)
-                ui.draggable.detach().appendTo($this).addClass('drawn');
-        },
+            if ( $this.children().length == 0 ) {
+                ui.draggable
+                    .detach()
+                    .css({top: 0,left: 0})
+                    .addClass('drawn')
+                    .appendTo($this);
+
+                refreshScoreBoard();
+            }
+        }
     });
 
-    $("#rack").droppable({
-        accept: ".drawn",
-        tolerance: "intersect",
-        hoverClass: "drop-hover",
-        drop: function(e, ui) {
+    $('#rack').droppable({
+        accept: '.drawn',
+        tolerance: 'intersect',
+        hoverClass: 'drop-hover',
+        drop: function (e, ui) {
             var $this = $(this);
-            ui.draggable.removeClass('drawn').detach().appendTo($this);
-        },
+            ui.draggable.detach().removeClass('drawn').css({top:0, left:0}).appendTo($this);
+            refreshScoreBoard();
+        }
     });
-
+refreshScoreBoard();
 });
+
+function refreshScoreBoard() {
+
+    var strWord = "";
+    var score = 0;
+    var letterVal;
+    var letterMult = 1;
+    var wordMult = 1;
+
+    $('.drawn').each(function(){
+        var $this = $(this)
+        strWord += $this.attr('letter');
+
+        letterVal = parseInt($this.attr('value'), 10);
+        letterMult = parseInt($this.parent().attr('letter-mult'), 10);
+
+        score += (letterVal * letterMult);
+        wordMult *= parseInt($this.parent().attr('word-mult'), 10);
+    });
+
+    $('#word').text(strWord);
+    $('#score').text(score*wordMult);
+    $('#bag').text(ObjScrabble.bag.length);
+
+}
